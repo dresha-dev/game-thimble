aeccf5e1cd385ec97871e00d8e9dfbed8adc5218
adreyponamarev <andrey.ponamarev@maxymiser.com>
2019-06-20 08:04:03 +0200

    Code clean up MVP is production ready

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,46 +1,53 @@
 import React, { useState, useRef, useCallback } from 'react';
 import { NUMBER_OF_THIMBLES, ITEM_WIDTH, DEFAULT_SPEED, BALL_WINNING_POSITION } from '../config';
-import { getOrderedArray, setZerosArray } from '../utils';
+import { getOrderedArray, setZerosArray, delay } from '../utils';
 import * as API from '../services/api';
 import Ball from '../components/Ball';
 
 const Board = () => {
-  const order = useRef(getOrderedArray(NUMBER_OF_THIMBLES));
-  const [positions, setPosition] = useState(setZerosArray(NUMBER_OF_THIMBLES));
-  const [speed] = useState(DEFAULT_SPEED);
-  const [ballWinningPosition, setBallPosition] = useState(BALL_WINNING_POSITION);
-  const [isBallVisible, setBallVisibility] = useState(true);
+  const order = useRef(getOrderedArray(NUMBER_OF_THIMBLES)); // Thimbles order
+  const [positions, setPosition] = useState(setZerosArray(NUMBER_OF_THIMBLES)); // Thimbles css position
+  const [speed] = useState(DEFAULT_SPEED); // Game speed
+  const [ballWinningPosition, setBallPosition] = useState(BALL_WINNING_POSITION); // Ball position under container
+  const [isBallVisible, setBallVisibility] = useState(true); // Shows ball visibillity
+  const [gameResult, setGameResult] = useState(''); // Game result message
+  const [gameStatus, setGameStatus] = useState('ready'); // Game sta
 
+  /**
+   * Reset game to the default state
+   */
   const resetGame = () => {
     order.current = getOrderedArray(NUMBER_OF_THIMBLES);
     setPosition(setZerosArray(NUMBER_OF_THIMBLES));
     setBallPosition(BALL_WINNING_POSITION);
+    setGameResult('');
   };
 
+  /**
+   * Load and animate shuffle path
+   */
   const startGame = async () => {
-    // Load data
-    resetGame();
-
-    const path = await API.getPath();
-
-    // LOGIC OF THE GAME
-    // input |   order       |  position // index - value
-    //       |  [0,1,2]      |  [ 0, 0, 0]
-    // [0,2] |  [2,1,0]      |  [-2, 0, 2]
-    // [2,1] |  [1,2,0]      |  [-1,-1, 2]
-    // [0,1] |  [2,1,0]      |  [-2, 0, 2]
-
-    // Hide ball
-
-    // Shuffle
-    await new Promise(resolve => {
-      draw(path, resolve);
-    });
-
-    // Where is the ball?
-    console.log('Where is the ball?');
+    if (gameStatus === 'loading') {
+      return;
+    }
+    setGameStatus('loading'); // Prevent user clicks
+    resetGame(); // Reset components to starting position
+    const path = await API.getPath(); // Get path from server
+    setBallVisibility(false); // Hide ball
+    await delay(500); // Delay is needed for hiding ball before shuffle
+    await new Promise(resolve => draw(path, resolve)); // Animate shuffle
+    setGameStatus('loaded'); // Allow user choose thimble
   };
 
+  /**
+   * Calculate thimbles and ball position
+   * @example
+   *  move |   order       |  position
+   *       |  [0,1,2]      |  [ 0, 0, 0] // Default position
+   * [0,2] |  [2,1,0]      |  [-2, 0, 2]
+   * [2,1] |  [1,2,0]      |  [-1,-1, 2]
+   * [0,1] |  [2,1,0]      |  [-2, 0, 2]
+   */
   const getNextPositions = useCallback((first, second) => {
     const currentOrder = order.current;
     const valueOfFirst = currentOrder[first];
@@ -76,31 +83,32 @@ const Board = () => {
     [getNextPositions, speed]
   );
 
-  const handleThimbleClick = selectedThimble => {
+  const handleThimbleClick = async selectedThimble => {
+    setBallVisibility(true);
+    await delay(300);
+
     if (selectedThimble === BALL_WINNING_POSITION) {
-      // You won!!!
+      setGameResult('Awesome! 🤩');
     } else {
-      // You lose!!!
+      setGameResult('Ops! 😫');
     }
+    setGameStatus('ready');
   };
 
   return (
     <div>
-      <button onClick={startGame}>Play!!!</button>
-      <button
-        onClick={() => {
-          setBallVisibility(!isBallVisible);
+      <div
+        style={{
+          position: 'relative',
+          pointerEvents: gameStatus === 'loading' || gameStatus === 'ready' ? 'none' : 'auto'
         }}
       >
-        Play again!!!
-      </button>
-
-      <div style={{ position: 'relative' }}>
         <div
           style={{
             position: 'relative',
             transition: `top 500ms`,
-            top: isBallVisible ? 0 : `${ITEM_WIDTH / 2 + 10}px`
+            top: isBallVisible ? 0 : `${ITEM_WIDTH / 2 + 10}px`,
+            marginBottom: `${ITEM_WIDTH / 2 + 10}px`
           }}
         >
           {getOrderedArray(NUMBER_OF_THIMBLES).map((key, index) => {
@@ -124,6 +132,8 @@ const Board = () => {
         </div>
         <Ball position={ballWinningPosition} />
       </div>
+      {gameResult !== '' && <div>{gameResult}</div>}
+      <button onClick={startGame}>Let's play!</button>
     </div>
   );
 };
--- client/src/utils.js
+++ client/src/utils.js
@@ -2,3 +2,4 @@ import _ from 'lodash';
 
 export const getOrderedArray = length => _.times(length, i => i);
 export const setZerosArray = length => _.times(length, () => 0);
+export const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
